f94f58dbcdb91626de5097a35f8decc150cfa37e
k3liang <130005349+k3liang@users.noreply.github.com>
2023-05-10 16:31:50 -0700

    update grading script and tests

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,56 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testFilterForWord() {
+    List<String> lst = Arrays.asList("hello", "Moon", "MOON", "mon", "moon", "woah", "mon", "moon ", "moon");
+    List<String> result = ListExamples.filter(lst, new IsMoon());
+    List<String> expected = Arrays.asList("Moon", "MOON", "moon", "moon");
+    assertEquals(expected, result);
+  }
+
+  @Test(timeout = 500)
+  public void testFilterForLongWords() {
+    List<String> lst = Arrays.asList("I", "want", "sleep", "not", "programming");
+    List<String> result = ListExamples.filter(lst, (s) -> s.length() >= 4);
+    List<String> expected = Arrays.asList("want", "sleep", "programming");
+    assertEquals(expected, result);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeInterwoven() {
+    List<String> left = Arrays.asList("a", "c", "e");
+    List<String> right = Arrays.asList("b", "d", "f");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "f");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeWithMoreInFirst() {
+    List<String> left = Arrays.asList("a", "c", "e", "g", "h", "i");
+    List<String> right = Arrays.asList("b", "d", "f");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeWithMoreInSecond() {
+    List<String> left = Arrays.asList("a", "c", "e");
+    List<String> right = Arrays.asList("b", "d", "f", "g", "h", "i");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeWithDuplicates() {
+    List<String> left = Arrays.asList("a", "c", "c", "d", "e");
+    List<String> right = Arrays.asList("b", "b", "d", "e", "f", "f");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "b", "c", "c", "d", "d", "e", "e", "f", "f");
+    assertEquals(expected, merged);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,6 +8,46 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+echo ""
+echo "------------------------------"
+file=`find student-submission -name "ListExamples.java"`
+if [[ -f $file ]]
+then
+    echo "FILE FOUND: ListExamples.java"
+else
+    echo "ERROR: FILE NOT FOUND: ListExamples.java"
+    echo "------------------------------"
+    exit 1
+fi
+
+cp TestListExamples.java grading-area
+cp $file grading-area
+
+# cd grading-area
+dir="grading-area"
+javac -cp $CPATH $dir/*.java > $dir/out.txt 2>&1
+if [[ $? != 0 ]]
+then
+    echo "COMPILE FAILURE: ListExamples.java"
+    cat $dir/out.txt
+    echo "------------------------------"
+    exit 1
+fi
+echo "COMPILE SUCCESS: ListExamples.java"
+java -cp "$dir;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > $dir/out2.txt 2>&1
+
+grep "Tests run" $dir/out2.txt > $dir/fails.txt
+grep "OK" $dir/out2.txt > $dir/success.txt
+grep "(TestListExamples)" $dir/out2.txt > $dir/methodfails.txt
+echo "------------------------------"
+echo "------------------------------"
+echo "Score: "
+cat $dir/success.txt
+cat $dir/fails.txt
+echo ""
+echo "Failed Tests: "
+cat $dir/methodfails.txt
+echo "------------------------------"
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
